16886556c208cf1c8a8588d5875f3269feeefa2a
arguilla <arguilla@student.42.fr>
2021-11-14 11:54:42 +0100

    create_token to create a new token

---
+++ srcs/lexer/create_token.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_token.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arguilla <arguilla@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/14 07:16:37 by arguilla          #+#    #+#             */
+/*   Updated: 2021/11/14 10:17:12 by arguilla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/*
+**	This function create a new token.
+**
+**	@param	data	=>	the data that will be stored in the token.
+**	@param	type	=>	the type of the data.
+**
+**	return	a new token.
+*/
+
+t_token	*create_token(char	*data, enum e_token_type type)
+{
+	t_token	*token;
+
+	token = alloc(sizeof(t_token), &g_shell->a);
+	token->data = data;
+	token->type = type;
+	token->next = NULL;
+	return (token);
+}
