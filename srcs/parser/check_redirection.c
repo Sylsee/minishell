0594352e5229b36329a5afbe425b3bd8a8382656
arguilla <arguilla@student.42.fr>
2021-11-28 13:37:34 +0100

    add check_redirection function to check if a redirection have the good syntax

---
+++ srcs/parser/check_redirection.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_redirection.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arguilla <arguilla@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 18:02:55 by arguilla          #+#    #+#             */
+/*   Updated: 2021/11/28 12:57:33 by arguilla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/*
+**	Check if the current token is a redirection and if the next token is valid
+**	for a redirections pattern.
+**
+**	@param	tokens	=>	the list of tokens.
+**
+**	@return	a boolean value.
+*/
+
+t_bool	check_redirection(t_token **tokens)
+{
+	if (is_redirection((*tokens)->type) && (!(*tokens)->next
+			|| (*tokens)->next->type != Word_token))
+	{
+		skip_token(tokens);
+		if (!*tokens)
+			ft_dprintf(STDERR_FILENO, "%s", error_msg(ERR_EOL));
+		if (!*tokens || (*tokens)->type != Word_token)
+			return (false);
+	}
+	return (true);
+}
