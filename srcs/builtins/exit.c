ab4ffc714bd9cf1e2e6d7dde3618dfba5a96db2b
Sylsee <sylvio.poliart@gmail.com>
2021-12-28 15:25:51 +0100

    fix exit value (must be unsigned char)

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: spoliart <spoliart@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 18:31:19 by spoliart          #+#    #+#             */
-/*   Updated: 2021/12/27 16:25:47 by spoliart         ###   ########.fr       */
+/*   Updated: 2021/12/28 15:08:28 by spoliart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,9 @@ int	run_exit(int argc, char **argv)
 	}
 	ft_putstr_fd("exit\n", STDERR_FILENO);
 	if (argc == 1)
-		exit(g_shell->exit_code);
-	else if (ft_isnumeric(argv[1]) == 0)
+		exit((unsigned char)g_shell->exit_code);
+	else if (ft_isnumeric(argv[1]) == 0 && argv[1][0] != '-' &&
+		argv[1][0] != '+' && ft_isnumeric(&argv[1][1]) == 0)
 	{
 		ft_dprintf(STDERR_FILENO, "minishell: exit: %s: numeric argument "
 			"required\n", argv[1]);
@@ -39,6 +40,6 @@ int	run_exit(int argc, char **argv)
 		ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
 		return (1);
 	}
-	exit(ft_atoi(argv[1]));
+	exit((unsigned char)ft_atoi(argv[1]));
 	return (0);
 }
